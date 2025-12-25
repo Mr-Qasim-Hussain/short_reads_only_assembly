cb95033db991bca0794e612be4260bd6b7e215c0
Mr-Qasim-Hussain <qasimhussain470@yahoo.com>
2025-12-25 21:27:13 +0800

    fastp and further quality control of processed reads

--- analysis.sh
+++ analysis.sh
@@ -37,3 +37,27 @@ conda activate 02_multiqc
 # run multiqc on fastqc files
 # expert use case (check file paths)
 multiqc -p -o reports "${PROJECT_DIR}/01_qc_before_processing/reports"
+
+# run fastp for read trimming and filtering with phred score threshold of 25
+cd "${PROJECT_DIR}/02_processed_reads"
+conda activate 01_short_read_qc
+fastp \
+  -i "${PROJECT_DIR}/00_raw_reads/codanics_1.fastq.gz" \
+  -I "${PROJECT_DIR}/00_raw_reads/codanics_2.fastq.gz" \
+  -o codanics_1_trimmed.fastq.gz \
+  -O codanics_2_trimmed.fastq.gz \
+  -q 25 -w 8 --detect_adapter_for_pe \
+  --html "${PROJECT_DIR}/03_qc_after_processing/fastp_report.html" \
+  --json "${PROJECT_DIR}/03_qc_after_processing/fastp_report.json"
+
+# fastqc and multiqc run on processed reads
+cd "${PROJECT_DIR}/03_qc_after_processing"
+conda activate 01_short_read_qc
+mkdir -p reports_fastqc_processed
+fastqc -o reports_fastqc_processed --extract --svg -t 8 "${PROJECT_DIR}/02_processed_reads/"*.fastq.gz
+
+# multiqc (to combine all reports)
+conda activate 02_multiqc
+multiqc -p -o reports_fastqc_processed "${PROJECT_DIR}/03_qc_after_processing/reports_fastqc_processed"
+
+echo "Short reads processing and QC complete!"
